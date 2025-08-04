e761f4df1f6ed6203ba76c278abb0ce59fa8e18b
Gothe Chander <gotheder1999@gmail.com>
2025-08-04 22:02:35 +0000

    Te añado en public el logo pero en .svg, usalo para ver si tiene alguna

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2 text-2xl font-headline font-bold">
-      <Image src="/Logo.webp" alt="ConhdeHelena Logo" width={40} height={40} />
+      <Image src="/Logo.svg" alt="ConhdeHelena Logo" width={40} height={40} />
       <span className="bg-gradient-to-r from-primary to-accent bg-clip-text text-transparent">
         ConhdeHelena
       </span>
