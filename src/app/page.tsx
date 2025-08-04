3812751409dc6c802071eededbee0468cdcb495a
Gothe Chander <gotheder1999@gmail.com>
2025-08-04 22:13:48 +0000

    No has arreglado nada y has roto el contenido de las demas paginas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,13 @@ import { AnimatedSection } from "@/components/animated-section";
 
 export default function Home() {
   return (
-    <>
-      <AnimatedSection key="hero">
+    <div>
+      <AnimatedSection>
         <HeroSection />
       </AnimatedSection>
-      <AnimatedSection key="featured-products">
+      <AnimatedSection>
         <FeaturedProductsSection />
       </AnimatedSection>
-    </>
+    </div>
   );
 }
