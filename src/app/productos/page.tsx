db9732b47789837397687d0dde879acfeb40db2f
Gothe Chander <gotheder1999@gmail.com>
2025-08-04 21:34:46 +0000

    Revisa la página productos, no quiero que haya duplicado

--- src/app/productos/page.tsx
+++ src/app/productos/page.tsx
@@ -1,13 +1,10 @@
-import { FeaturedProductsSection } from "@/components/landing/featured-products-section";
+
 import { ProductListSection } from "@/components/landing/product-list-section";
 import { AnimatedSection } from "@/components/animated-section";
 
 export default function ProductosPage() {
   return (
     <>
-      <AnimatedSection>
-        <FeaturedProductsSection />
-      </AnimatedSection>
       <AnimatedSection>
         <ProductListSection />
       </AnimatedSection>
